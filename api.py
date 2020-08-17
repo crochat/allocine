3c80dab95572eaa181d8a079acfafbe724a34e45
Cedric Rochat <rochatc@gmail.com>
2020-08-17 12:00:22 +0200

    New keys + new signature method

--- api.py
+++ api.py
@@ -88,13 +88,14 @@ class allocine(object):
         today = date.today()
         sed = today.strftime('%Y%m%d')
         #print(sed)
-        to_hash = (self._secret_key + urllib.parse.urlencode(params) + '&sed=' + sed).encode("utf8")
+        params['sed'] = sed
+        to_hash = (method + urllib.parse.urlencode(params) + self._secret_key).encode("utf8")
         sha1 = hashlib.sha1(to_hash).digest()
         #print(sha1)
-        b64 = base64.b64encode(sha1)
-        #print(b64)
-        sig = urllib.parse.quote(b64)
-        query_url += '?'+urllib.parse.urlencode(params, True)+'&sed='+sed+'&sig='+sig
+        sig = base64.b64encode(sha1)
+        #print(sig)
+        params['sig'] = sig
+        query_url += '?'+urllib.parse.urlencode(params, True)
         #print(query_url)
 
         triesmax = 10
